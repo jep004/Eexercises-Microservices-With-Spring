f135f0b881c69873b0d77b5f6c7159d91718bd6b
Esteve Verge <jnev@gft.com>
2020-05-06 11:54:35 +0200

    Primer commit

---
+++ lab-1-spring-boot/src/main/java/demo/controller/InitialController.java
@@ -0,0 +1,14 @@
+package demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InitialController {
+	
+	@GetMapping("/")
+	public String start() {
+		return "Hello";
+	}
+
+}
